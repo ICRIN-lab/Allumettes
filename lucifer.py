d13a77bdcd7a349ef2dd9988067b9bb2f4add9b8
mselleki <mselleki98@gmail.com>
2022-04-16 16:05:28 +0200

    final version wit

--- lucifer.py
+++ lucifer.py
@@ -30,7 +30,7 @@ class Lucifer(TaskTemplate):
         f"de droite. \n\n - appuyez sur la touche '{no_key_name}' pour selectionner la réponse de "
         f"gauche.",
         "N'appuyez sur les touches que lorsqu'on vous le demande.",
-        f"Placez vos index sur les touches de couleurs ({no_key_name} et {yes_key_name})."
+        f"Placez vos index sur les touches {no_key_name} et {yes_key_name}."
     ]
     csv_headers = [
         "no_trial",
