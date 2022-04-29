ab4a0525913fa4447fea85c01c1d03f131592aac
mselleki <mselleki98@gmail.com>
2022-04-29 11:09:50 +0200

    final version lucifer

--- lucifer.py
+++ lucifer.py
@@ -11,7 +11,7 @@ from list_ans import L_ans, L_img
 class Lucifer(TaskTemplate):
     # IMPORTANT ! To MODIFY IF NEEDED
     nb_ans = 2
-    response_pad = True  # has to be set on "True" on production.
+    response_pad = False  # has to be set on "True" on production.
     # END OF IMPORTANT
     yes_key_name = "verte"
     yes_key_code = "6"
@@ -54,7 +54,7 @@ class Lucifer(TaskTemplate):
         core.wait(waiting_time)
         seed = random.randint(0, 1)
         left_ans = L_ans[no_trial][seed]
-        self.create_visual_text(text=f"Combien d'allumettes avez-vous vu ? ( {L_ans[no_trial].pop(seed)} / {L_ans[no_trial][0]})").draw()
+        self.create_visual_text(text=f"Combien d'allumettes avez-vous vu ? ({L_ans[no_trial].pop(seed)} / {L_ans[no_trial][0]})").draw()
         self.win.flip()
         if seed:
             good_ans = self.yes_key_code
